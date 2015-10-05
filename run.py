cb683ba7a54e2a34b8d05585b02bc4f7cdd23d22
Bang Dao <bang.dao@eastagile.com>
2015-10-05 17:01:34 +0700

    add run

---
+++ run.py
@@ -0,0 +1,14 @@
+import os
+import random
+import pdb
+branchs = 10
+commits = 20
+for branch in range(branchs):
+	os.system("git co master")
+	branch_name = [random.choice(["do", "some", "thing", "new", "as", "client", "requirement"]) for x in range(6)]
+	os.system("git co -b feature/%s-%s" % (branch, "-".join(branch_name)))
+	for commit in range(commits):
+		os.system("touch %s-%s" % (branch, commit))
+		os.system("git add .")
+		commit_name = " ".join([random.choice(["add", "new", "thing", "to", "current", "branch", "with", "code"]) for x in range(6)])
+		os.system("git commit -m \"[f] %s - %s\"\n\n%s" % (branch, " ".join(branch_name), commit_name))
